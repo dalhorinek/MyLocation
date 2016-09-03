cd005b2bd884bd458009ec442aa3e3ff3920f181
Dalibor Horinek <dal@horinek.net>
2016-09-03 13:18:02 +0200

    Initial Commit

---
+++ MyLocation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyLocation
+//
+//  Created by Dalibor Horinek on 03/09/2016.
+//  Copyright © 2016 Dalibor Horinek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
