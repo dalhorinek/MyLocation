a008d72482400224280d85cdfe49fb9dfd7c0711
Dalibor Horinek <dal@horinek.net>
2016-09-03 13:19:09 +0200

    Implement MyLocation

--- MyLocation/main.swift
+++ MyLocation/main.swift
@@ -7,6 +7,65 @@
 //
 
 import Foundation
+import CoreLocation
 
-print("Hello, World!")
+class MyLocation: NSObject, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .notDetermined {
+            return
+        }
+        
+        if status != .authorizedAlways {
+            exit(3)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count > 0 {
+            let location: CLLocation = locations[0]
+            let coordinate = location.coordinate
+            print("\(coordinate.latitude) \(coordinate.longitude)")
+            geocoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                let possibleAddress: CLPlacemark? = placemarks?.first
+                
+                if let address = possibleAddress {
+                    print("\(address.name!) \(address.administrativeArea!) \(address.country!)")
+                    exit(0)
+                }
+            })
+        }
+    }
+    
+    func start() {
+        let authStatus: CLAuthorizationStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == CLAuthorizationStatus.restricted || authStatus == CLAuthorizationStatus.denied {
+            print("Sorry, location is not permited, you need to enable it in System Preferences")
+            exit(1)
+        }
+        
+        if CLLocationManager.locationServicesEnabled() != true {
+            print("Sorry, location not supported")
+            exit(2)
+        }
+        
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.startUpdatingLocation()
+    }
+}
 
+let ml = MyLocation()
+ml.start()
+
+autoreleasepool({
+    var runLoop: RunLoop = RunLoop.main()
+    runLoop.run()
+})
